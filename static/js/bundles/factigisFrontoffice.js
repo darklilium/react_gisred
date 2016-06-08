9c2fa83bd6c2eff02dd1baa76e8a234a963a3889
Evelyn Hernández <dark.desires@live.cl>
2016-06-08 11:25:20 -0400

    Adding components to frontoffice factigis

---
+++ static/js/bundles/factigisFrontoffice.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Factigis from '../components/factigis/Factigis.jsx';
+
+ReactDOM.render(<Factigis />, document.getElementById('myFactigis_FrontOffice'));
